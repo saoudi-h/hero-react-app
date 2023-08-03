be74bc86f4a58ebc622f16a3101bef0257bfa693
Hakim Saoudi <saoudihakim@gmail.com>
2023-08-03 16:55:12 +0200

    add HerosList

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Carte from './components/carte/Carte';
+import Carte, { Heros } from './components/carte/Carte';
 import Cartes from './components/cartes/Cartes';
 import Compteur from './components/compteur/Compteur';
+import HerosLists from './components/herosLists/HerosLists';
+import Data from './data/Data';
 function App() {
+  const allHeros:Heros[] = new Data().getAll();
   return (
     <div className="App">
       <Header />
       {/* <Carte id={1} name='fatigman' civil='hakim' age={32} image='https://avatars.githubusercontent.com/u/68503582?v=4' ville='Oyonnax' /> */}
-      <Cartes />
+      {/* <Cartes /> */}
+      <HerosLists allHeros={allHeros}/>
       <Compteur />
       <Footer />
     </div>
